357e74d68bb75662a5045d686fa52586a8ce943c
Michael Cole <michaelcole@michaelcole.com>
2014-08-30 18:33:51 -0500

    Add animation, remove DateSprite, cleanup to start server side

--- packages/dashboard/public/controllers/dashboard.js
+++ packages/dashboard/public/controllers/dashboard.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var app = angular.module('mean.dashboard', ['ui.bootstrap']);
+var app = angular.module('mean.dashboard', ['ui.bootstrap', 'ngAnimate']);
 
 app.controller('DashboardController', ['$scope', 'Global', 'Dashboard',
   function($scope, Global, Dashboard) {
@@ -16,29 +16,37 @@ app.controller('DashboardController', ['$scope', 'Global', 'Dashboard',
       { date: 'Jan 1st',
         calories: 3400,
         display: 'green',
-        showDateSprite: true,
+        showDateSprite: false,
         entries: [
           {
             date: 'Jan 1st',
             time: '12pm',
+            filterDate: true,
+            filterTime: true,
             description: 'Lunch ad Midas Touch',
             calories: '1200'
           },
           {
             date: 'Jan 1st',
             time: '1pm',
+            filterDate: true,
+            filterTime: true,
             description: 'Pastry at La Boulangerie',
             calories: '800'
           },
           {
             date: 'Jan 1st',
             time: '3pm',
+            filterDate: true,
+            filterTime: true,
             description: 'Horchata tea',
             calories: '200'
           },
           {
             date: 'Jan 1st',
             time: '5pm',
+            filterDate: true,
+            filterTime: true,
             description: 'Burger and salad at Charlito\'s',
             calories: '1200'
           }
@@ -46,30 +54,76 @@ app.controller('DashboardController', ['$scope', 'Global', 'Dashboard',
       },
       { date: 'Jan 2nd',
         calories: 3400,
-        showDateSprite: true,
+        showDateSprite: false,
         display: 'red',
         entries: [
           {
-            date: 'Jan 1st',
+            date: 'Jan 2nd',
             time: '12pm',
+            filterDate: true,
+            filterTime: true,
             description: 'Lunch ad Midas Touch',
             calories: '1200'
           },
           {
-            date: 'Jan 1st',
+            date: 'Jan 2nd',
             time: '1pm',
+            filterDate: true,
+            filterTime: true,
             description: 'Pastry at La Boulangerie',
             calories: '800'
           },
           {
-            date: 'Jan 1st',
+            date: 'Jan 2nd',
             time: '3pm',
+            filterDate: true,
+            filterTime: false,
             description: 'Horchata tea',
             calories: '200'
           },
           {
-            date: 'Jan 1st',
+            date: 'Jan 2nd',
             time: '5pm',
+            filterDate: false,
+            filterTime: true,
+            description: 'Burger and salad at Charlito\'s',
+            calories: '1200'
+          }
+        ]
+      },      { date: 'Jan 3rd',
+        calories: 3400,
+        showDateSprite: false,
+        display: 'red',
+        entries: [
+          {
+            date: 'Jan 3rd',
+            time: '12pm',
+            filterDate: true,
+            filterTime: true,
+            description: 'Lunch ad Midas Touch',
+            calories: '1200'
+          },
+          {
+            date: 'Jan 3rd',
+            time: '1pm',
+            filterDate: true,
+            filterTime: true,
+            description: 'Pastry at La Boulangerie',
+            calories: '800'
+          },
+          {
+            date: 'Jan 3rd',
+            time: '3pm',
+            filterDate: true,
+            filterTime: false,
+            description: 'Horchata tea',
+            calories: '200'
+          },
+          {
+            date: 'Jan 3rd',
+            time: '5pm',
+            filterDate: false,
+            filterTime: true,
             description: 'Burger and salad at Charlito\'s',
             calories: '1200'
           }
