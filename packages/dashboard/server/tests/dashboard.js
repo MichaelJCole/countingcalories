5bd945f7d9f19021c2ac83957f1fe4b7e76c29bb
Michael Cole <michaelcole@michaelcole.com>
2014-09-04 16:49:14 -0500

    Test cases

--- packages/dashboard/server/tests/dashboard.js
+++ packages/dashboard/server/tests/dashboard.js
@@ -7,6 +7,7 @@ var should = require('should');
 var assert = require('assert');
 var request = require('supertest');  
 var mongoose = require('mongoose');
+var _ = require('lodash');// jshint ignore:line
 var User = mongoose.model('User');
 var Goal = mongoose.model('Goal');
 var Journal = mongoose.model('Journal');
@@ -14,8 +15,11 @@ var Journal = mongoose.model('Journal');
 /**
  * Globals
  */
+var serverURL = 'http://localhost:3000';
 var user;
-var serverURL = 'http://localhost:3001';
+var sessionCookie;
+
+
 /**
  * Test Suites
  */
@@ -25,13 +29,12 @@ describe('<Unit Test>', function() {
       // Add the user to the database through mongoDB
       user = new User({
         name: 'Full name',
-        email: 'test99@test.com',
+        email: 'test@michaelcole.com',
         username: 'user',
         password: 'password'
       });
 
       user.save(function() {
-        // After saved, we're done.
         done();
       });
     });
@@ -44,48 +47,140 @@ describe('<Unit Test>', function() {
     });
 
     describe('Goal API:', function() {
-      it('should create goals for user', function(done) {
 
-        // Login as user
-        // - should return auth cookie
+
+      // Test server is up and we can get home page
+      it('should create get the home page', function(done) {
+
         request(serverURL)
-        .post('/login')
-        .send({email: user.email, password: user.password })
-        // end handles the response
-            .end(function(err, res) {
-              if (err) {
-                throw err;
-              }
-              // this is should.js syntax, very clear
-              res.should.have.status(200);
-        });
-
-        // GET: <base>/goal
-        // - should return ""
-
-        // POST: <base>/goal  w/ goal = 2000
-        // - should return json of goal
-
-        // GET: <base>/goal
-        // - should return 2000
-
-        // POST: <base>/goal  w/ invalid goal -1
-        // - should return error
-
-        // GET: <base>/goal
-        // - should return 2000
-
-        // Logout
-        // - ok
-
-        // POST: <base>/goal  w/ 2000
-        // - should return authentication error
-
-        // GET: <base>/goal
-        // - should return authentication error
-
-        done();
+          .get('/')
+          .end(function(err, res){
+            _.delay(function(){
+              done();
+            },500);
+          });
       });
+
+      // Test the user can logout
+      it('should logout the user', function(done) {
+        request(serverURL)
+          .get('/logout')
+          // end handles the response
+          .end(function(err, res) {
+            if (err) {
+              throw err;
+            }
+            // this is should.js syntax, very clear
+            res.should.have.status(302);
+            done();
+          });
+      });
+
+      // Login the user
+      it('should login the user', function(done) {
+        request(serverURL)
+          .post('/login')
+          .send({email: user.email, password: user.password })
+          // end handles the response
+          .end(function(err, res) {
+            if (err) {
+              throw err;
+            }
+            sessionCookie = res.headers['set-cookie'];
+            console.log(sessionCookie);
+            // this is should.js syntax, very clear
+            res.should.have.status(200);
+            done();
+          });
+      });
+
+      // GET: <base>/goal
+      // - should return json of goal
+      it('should get the goal', function(done) {
+        request(serverURL)
+          .get('/dashboard/api/1.0/goal')
+          .set('Cookie', sessionCookie)
+          // end handles the response
+          .end(function(err, res) {
+            if (err) {
+              throw err;
+            }
+            // this is should.js syntax, very clear
+            res.should.have.status(200);
+            done();
+          });
+      });
+
+      // POST: <base>/goal
+      // - should return 2000
+      it('should set the goal to 2000', function(done) {
+        request(serverURL)
+          .post('/dashboard/api/1.0/goal')
+          .set('Cookie', sessionCookie)
+          .send({goal: 2000 })
+          // end handles the response
+          .end(function(err, res) {
+            if (err) {
+              throw err;
+            }
+            // this is should.js syntax, very clear
+            res.should.have.status(200);
+            done();
+          });
+      });
+
+
+      // GET: <base>/goal and check it's 2000
+      // - should return json of goal
+      it('should get the goal and check its 2000', function(done) {
+        request(serverURL)
+          .get('/dashboard/api/1.0/goal')
+          .set('Cookie', sessionCookie)
+          // end handles the response
+          .end(function(err, res) {
+            if (err) {
+              throw err;
+            }
+            // this is should.js syntax, very clear
+            res.should.have.status(200);
+            res.body.should.have.property('goal');
+            res.body.goal.should.equal(2000);                    
+            done();
+          });
+      });
+
+      // POST: <base>/goal  w/ invalid goal -1
+      // - should return error
+      it('should fail if the goal is set incorrectly e.g. -1', function(done) {
+        request(serverURL)
+          .post('/dashboard/api/1.0/goal')
+          .set('Cookie', sessionCookie)
+          .send({goal: -1 })
+          // end handles the response
+          .end(function(err, res) {
+            if (err) {
+              throw err;
+            }
+            // this is should.js syntax, very clear
+            res.should.have.status(200);  // TODO: fixme!
+            done();
+          });
+      });
+
+
+      // GET: <base>/goal
+      // - should return 2000
+
+      // Logout
+      // - ok
+
+      // POST: <base>/goal  w/ 2000
+      // - should return authentication error
+
+      // GET: <base>/goal
+      // - should return authentication error
+
+
     });
 
     describe('Journal API:', function() {
